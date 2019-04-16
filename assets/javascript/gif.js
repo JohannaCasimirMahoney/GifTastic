0c9f475069d271530800577def51ea724d0b7eca
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-04-15 20:09:34 -0700

    pseduo code

---
+++ assets/javascript/gif.js
@@ -0,0 +1,7 @@
+// TODO: Create user clicks on button / non - animated gif images-- >
+// TODO: Create user clicks on the STILL GHIPY images-- >
+// TODO: Place GIPGY API on page-- >
+// TODO: Under evey gif, diplay its rating-- >
+// TODO: Add a form to the page-- >
+// TODO: Create vaule for the form for user input box and adds it into my 'Topic array' -- >
+// TODO: Make function call that each topic in the array, remkaes the button on page-- >
