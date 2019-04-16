ac4dfb581852f8755ca639a2ac34fcd7f339eedd
Ghost <ghost@Ghosts-MacBook-Pro.local>
2019-04-16 15:23:27 -0700

    added APIKey

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -18,18 +18,19 @@ $('document').ready(function () {
 }
 
 
-// TODO: Get API Key-- >
+// TODO: Place GIPGY API on page-- >
+var APIKey = "nPMtSnokNV7yOlwbU1DYxwYcywrpFTJq";
 // TODO: Create user clicks on button / non - animated gif images-- >
 // TODO: Create user clicks on the STILL GHIPY images-- >
-// TODO: Place GIPGY API on page-- >
+
 // TODO: Creat AJAX URL
-   $.ajax({
+$.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
     console.log(response);
 });
-// TODO: Under evey gif, diplay its rating-- >
+// TODO: Under every gif, diplay its rating-- >
 // TODO: Add a form to the page-- >
 // TODO: Create vaule for the form for user input box and adds it into my 'Topic array' -- >
 // TODO: Make function call that each topic in the array, remkaes the button on page-- >
