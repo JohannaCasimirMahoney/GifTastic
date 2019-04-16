8965cd3f21377cdcaba742c081eb7a92ad3dd939
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-04-15 23:10:22 -0700

    added var topic and names

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,10 +1,28 @@
 
 // TODO: Create docu ready function
-// TODO: Create an Array of Strings.Save it to a Variable called 'Topic' -- >
+$('document').ready(function () {
+    // TODO: Create an Array of Strings.Save it to a Variable called 'Topic' -- >
+    var topic = [
+
+        'bugs bunny',
+        'elmyra duff',
+        'tasmanian devil',
+        'porky pig',
+        'elmer fudd',
+        'syvester the cat',
+        'daffy duck',
+        'yosemite same',
+        'tweety bird',
+        'wile e coyote'];
+
+}
+
+
 // TODO: Get API Key-- >
 // TODO: Create user clicks on button / non - animated gif images-- >
 // TODO: Create user clicks on the STILL GHIPY images-- >
 // TODO: Place GIPGY API on page-- >
+// TODO: Creat AJAX URL
 // TODO: Under evey gif, diplay its rating-- >
 // TODO: Add a form to the page-- >
 // TODO: Create vaule for the form for user input box and adds it into my 'Topic array' -- >
