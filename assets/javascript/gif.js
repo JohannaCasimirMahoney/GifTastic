0141c8556fd7897d4fde4c9f70c5cc3dee2273dc
Ghost <ghost@campus-073-022.ucdavis.edu>
2019-04-20 09:58:03 -0700

    add gif url

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -25,20 +25,40 @@ for (var i = 0; i < topics.length; i++) {
 // TODO: Make function call that each topic in the array, remkaes the button on page-- >
 
 // TODO: Create vaule for the form for user input box and adds it into my 'Topic array' -- >
+// $(document).on("click", ".yourClassNameYouWantTOTarget", function () {
+//code goes here
+// })
 
 
+var gifSearch = "tiny toons adventures";
 
 // TODO: Place GIPGY API on page-- >
-var APIKey = "nPMtSnokNV7yOlwbU1DYxwYcywrpFTJq";
-// TODO: Create user clicks on button / non - animated gif images-- >
-// TODO: Create user clicks on the STILL GHIPY images-- >
-
+var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=nPMtSnokNV7yOlwbU1DYxwYcywrpFTJq&q=${gifSearch}&limit=10&offset=0&rating=G&lang=en`
 // TODO: Creat AJAX URL
 $.ajax({
     url: queryURL,
     method: "GET"
+
+    // After data comes back from the request
 }).then(function (response) {
-    console.log(response);
+    console.log("response check", response);
+
+    // Rating results
+    var p = $("<p>").text("Rating: " + results[i].rating);
+
+    // Creating and storing an image tag
+    var tinyToonsImage = $("<img>");
+
+
+
+
 });
+
+
+// TODO: Create user clicks on button / non - animated gif images-- >
+
+// TODO: Create user clicks on the STILL GHIPY images-- >
+
+
 
 
