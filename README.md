a5cd8bf912a743c763146fe1837d7c44ba185207
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-04-21 00:09:53 -0700

    updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # GifTastic
+
+### Overview
+
+In this assignment, you'll use the GIPHY API to make a dynamic web page that populates with gifs of your choice. To finish this task, you must call the GIPHY API and use JavaScript and jQuery to change the HTML of your site.
+
+Users may click on one of the buttons labeled with the title of a TinyToon cartoon character to view ten relevant GIFs from GIPHY. 
+
+The program initially displays the GIFs as still images and as ainmated.
--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -13,13 +13,13 @@ var topics = [
     "Foghorn Leghorn",];
 
 var button;
-var newTopic = ""; // new topic that will be added via the input field 
+var newTopic = ""; // new topic when added in the input field 
 
-// function to create new buttons from the topics array
+// Makes new buttons from the topics array
 var buttonGenerator = function () {
-    // the previous div elements are emptied 
+
     $("#buttonArea").empty();
-    // loops through the array and creates buttons
+    // loops through the array and creats buttons
     for (i = 0; i < topics.length; i++) {
         button = $("<button type=" + "button" + ">" + topics[i] + "</button>").addClass("btn btn-warning").attr("data", topics[i]);
         $("#buttonArea").append(button);
@@ -27,7 +27,7 @@ var buttonGenerator = function () {
 }
 
 
-// The user clicks on a generated orange button, which generates 10 static, non-animated gif images from the GIPHY API and places them on the page. 
+// The user clicks on  button, which generates 10 static, non-animated gif images from the GIPHY API and places on page. 
 $("#buttonArea").on("click", ".btn", function () {
     var thing = $(this).attr("data");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thing + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -52,7 +52,7 @@ $("#buttonArea").on("click", ".btn", function () {
             p.text(results[i].rating);
             var p = $("<p>").text("Rating: " + results[i].rating);
 
-            // add a CSS style to create colored borders around the gifs
+
             var topicImage = $("<img>").addClass("orangeBorder");
 
             // add states of animate and still which will be toggled 
@@ -92,7 +92,7 @@ $("#gifArea").on("click", ".gif", function (event) {
 })
 
 
-// The form takes the value from the input box and adds it into the topics  array. The buttonGenerator function is called that takes each topic in the array remakes the buttons on the page.
+// The form takes the value from the input box. Then adds it into the topics array. The buttonGenerator function is called that takes each topic in the array remakes the buttons on the page.
 
 
 $(".submit").on("click", function (event) {
